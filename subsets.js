fc2c5a0dc9d1d4f40b0ae7b8f72b934216034800
Dev Patel <dhpatel15@gmail.com>
2019-05-08 22:49:36 -0400

    subset

---
+++ subsets.js
@@ -0,0 +1,16 @@
+var subsets = function(nums) {
+  let result = [];
+  dfs([], 0);
+  
+  function dfs(current, index){
+
+      result.push(current);
+      for(let i = index; i < nums.length; i++) {
+          dfs(current.concat(nums[i]), i + 1);
+      }
+    
+    return
+  }
+  
+  return result;
+};
