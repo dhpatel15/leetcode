1a53ad017f2d1c415a10e1e580d717bea7da201c
Dev Patel <dhpatel15@gmail.com>
2019-08-01 23:48:58 -0400

    findPeakElement

---
+++ findPeakElement.js
@@ -0,0 +1,11 @@
+var findPeakElement = function(nums) {
+    if(nums.length ==1) return 0;
+    for(var i=0; i<nums.length; i++){
+        if((i==0 && nums[i]>nums[i+1]) || (i==nums.length-1 && nums[i]>nums[i-1])) {
+            return i;
+        }else if(nums[i]>nums[i+1] && nums[i]>nums[i-1]){
+            return i;
+        }
+    }
+    return -1;
+};
