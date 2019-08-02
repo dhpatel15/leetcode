75047638d6b12a4ac7dec7e1227d270a8764407c
Dev Patel <dhpatel15@gmail.com>
2019-08-02 18:11:08 -0400

    containsNearbyDuplicate

---
+++ containsNearbyDuplicate.js
@@ -0,0 +1,10 @@
+function containsNearbyDuplicate(nums, k) {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i - map[nums[i]] <= k) return true;
+    map[nums[i]] = i;
+  }
+
+  return false;
+}
