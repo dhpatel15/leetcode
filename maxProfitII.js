d8f18cce6b98ab590584af99284b221fcde9a211
Dev Patel <dhpatel15@gmail.com>
2020-04-13 21:49:09 -0400

    maxProfitII.js

--- maxProfitII.js
+++ maxProfitII.js
@@ -5,11 +5,10 @@ var maxProfitII = function(prices) {
   }
   
   let profit = 0;
-  
-  for(let i = 0; i < prices.length - 1; i++){
-    if(prices[i + 1] > prices[i]){
-      profit += prices[i + 1] - prices[i];
-    }
+
+  for (let i = 0; i < prices.length - 1; i++) {
+      const possibleProfit = prices[i + 1] - prices[i];
+      profit = Math.max(profit + possibleProfit, profit);
   }
   
   return profit;
